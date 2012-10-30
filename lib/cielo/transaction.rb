278e2085c18792ae3f7105ed1803ea71e9aab7eb
viniciusnz <viniciusnz@gmail.com>
2012-10-30 11:05:02 -0200

    VF

--- lib/cielo/transaction.rb
+++ lib/cielo/transaction.rb
@@ -7,8 +7,18 @@ module Cielo
     def create!(parameters={})
       analysis_parameters(parameters)
       message = xml_builder("requisicao-transacao") do |xml|
+        if parameters[:numero_cartao]
+          xml.tag!("dados-portador") do
+            xml.tag!(:numero, parameters[:numero_cartao].to_s)
+            
+            xml.tag!(:validade, parameters[:validade].to_s)
+            [:indicador, :"codigo-seguranca"].each do |key|
+              xml.tag!(key.to_s, parameters[key].to_s)
+            end
+          end
+        end
         xml.tag!("dados-pedido") do
-          [:numero, :valor, :moeda, :"data-hora", :idioma].each do |key|
+          [:numero, :valor, :moeda, :"data-hora", :descricao, :idioma, :"soft-descriptor"].each do |key|
             xml.tag!(key.to_s, parameters[key].to_s)
           end
         end
@@ -17,9 +27,11 @@ module Cielo
             xml.tag!(key.to_s, parameters[key].to_s)
           end
         end
-        xml.tag!("url-retorno", parameters[:"url-retorno"])
+        xml.tag!("url-retorno", parameters[:"url-retorno"]) if parameters[:"url-retorno"]
         xml.autorizar parameters[:autorizar].to_s
         xml.capturar parameters[:capturar].to_s
+        xml.tag!("campo-livre", parameters[:"campo-livre"]) if parameters[:"campo-livre"]
+        xml.tag!("bin", parameters[:bin]) if parameters[:bin]
       end
       make_request! message
     end
@@ -43,24 +55,30 @@ module Cielo
     
     private
     def analysis_parameters(parameters={})
-      [:numero, :valor, :bandeira, :"url-retorno"].each do |parameter|
+      [:numero, :valor, :bandeira].each do |parameter|
         raise Cielo::MissingArgumentError, "Required parameter #{parameter} not found" unless parameters[parameter]
       end
+      parameters.merge!(:validade => ["%04d" % parameters[:validade_ano], "%02d" % parameters[:validade_mes]].join("")) if parameters[:validade_ano]
+      parameters.merge!(:indicador => "1") unless parameters[:indicador]
       parameters.merge!(:moeda => "986") unless parameters[:moeda]
       parameters.merge!(:"data-hora" => Time.now.strftime("%Y-%m-%dT%H:%M:%S")) unless parameters[:"data-hora"]
+      parameters.merge!(:descricao => "") unless parameters[:indicador]
       parameters.merge!(:idioma => "PT") unless parameters[:idioma]
+      parameters.merge!(:"soft-descriptor" => "") unless parameters[:indicador]
       parameters.merge!(:produto => "1") unless parameters[:produto]
       parameters.merge!(:parcelas => "1") unless parameters[:parcelas]
       parameters.merge!(:autorizar => "2") unless parameters[:autorizar]
       parameters.merge!(:capturar => "true") unless parameters[:capturar]
       parameters.merge!(:"url-retorno" => Cielo.return_path) unless parameters[:"url-retorno"]
+      parameters.merge!(:"campo-livre" => "") unless parameters[:"campo-livre"]
+      parameters.merge!(:"bin" => parameters[:numero_cartao][0..5]) if parameters[:numero_cartao]
       parameters
     end
     
     def xml_builder(group_name, target=:after, &block)
       xml = Builder::XmlMarkup.new
       xml.instruct! :xml, :version=>"1.0", :encoding=>"ISO-8859-1"
-      xml.tag!(group_name, :id => "#{Time.now.to_i}", :versao => "1.1.0") do
+      xml.tag!(group_name, :id => "#{Time.now.to_i}", :versao => "1.2.0") do
         block.call(xml) if target == :before
         xml.tag!("dados-ec") do
           xml.numero Cielo.numero_afiliacao
